1f5125519f94fb3ea1127a0ff6bacb008f7eb65b
ThanhNga86 <108570482+ThanhNga86@users.noreply.github.com>
2024-04-07 22:17:33 +0700

    Delete soufoods_backend/soufoods directory

--- soufoods_backend/soufoods/src/main/java/com/soufoods/repo/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.soufoods.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.soufoods.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long>{
-
-}
