2f4fa3fecd5bc2d4c4fbbd5b0fcf19c8e272659b
Thanh Ngà <testtest86n@gmail.com>
2024-05-25 13:35:25 +0700

    file

--- soufoods_backend/soufoods/src/main/java/com/soufoods/repo/OrderDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.soufoods.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.soufoods.entity.OrderDetail;
-
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
-
-}
